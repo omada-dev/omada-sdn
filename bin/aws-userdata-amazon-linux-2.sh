9d8f23e731aa0176d0b3611db797a9db57f050d5
omada-dev <omada.sdn@proton.me>
2022-07-20 00:44:21 +0200

    add Amazon Linux 2 Userdata bash script

---
+++ bin/aws-userdata-amazon-linux-2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Copyright (c) 2022 Omada Community Developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# Omada Controller installer sources: https://community.tp-link.com/en/business/forum/topic/548752
+# Linux:            https://static.tp-link.com/upload/software/2022/202205/20220507/Omada_SDN_Controller_v5.3.1_Linux_x64.tar.gz
+
+OMADAURL="https://static.tp-link.com/upload/software/2022/202205/20220507/Omada_SDN_Controller_v5.3.1_Linux_x64.tar.gz" && echo "URL set: ${OMADAURL}"
+
+# Set vars
+OMADATARGZ=$(echo "${OMADAURL}" | awk '{split($0,a,"/"); print a[9]}') && echo "Omada installer filename set: ${OMADATARGZ}"
+MYINSTALLERFOLDER=$(echo "${OMADATARGZ}" | sed 's/\.tar\.gz//') && echo "Omada install folder set: ${MYINSTALLERFOLDER}"
+MYOMADAVERSION=$(echo "${OMADATARGZ}" | awk '{split($0,a,"_"); print a[4]}') && echo "Omada version: ${MYOMADAVERSION}"
+OMADADEST="/opt/tplink/EAPController"
+
+# Add mongodb repo
+cat <<"EOF" | tee /etc/yum.repos.d/mongodb-org-4.4.repo
+[mongodb-org-4.4]
+name=MongoDB 4.4 Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.4/x86_64
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
+EOF
+
+# yum update
+yum update -y
+yum install -y java-1.8.0-openjdk-headless jsvc mongodb-org
+
+# Download and install Omada
+# Check if omada installation folder exists, as if it does, no installation is
+# required
+if [[ -d ${OMADADEST} ]]; then
+    echo "Omada destination folder exists, skipping installation"
+else
+    if [[ -f ./${OMADATARGZ} ]]; then 
+        # If omada installer is found, extract it
+        echo "Omada installer file ${OMADATARGZ} found, skip download"
+        tar -xzf "${OMADATARGZ}"
+    else 
+        # If omada installer is not found, download and extract it
+        wget -c ${OMADAURL} -O - | tar -xz 
+    fi
+    # Make scripts and binaries executable    
+    chmod +x ${MYINSTALLERFOLDER}/*.sh
+    chmod +x ${MYINSTALLERFOLDER}/bin/*
+    # Install omada controller, by default it is enabled on boot
+    cd ${MYINSTALLERFOLDER}
+    ./install.sh
+fi
