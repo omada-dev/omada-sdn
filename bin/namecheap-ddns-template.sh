854b46acd010ee4047bf04b29b925707d8e2053e
omada-dev <omada.sdn@proton.me>
2022-08-26 07:02:14 +0200

    namecheap-ddns-template.sh - fix comments

--- bin/namecheap-ddns-template.sh
+++ bin/namecheap-ddns-template.sh
@@ -2,9 +2,9 @@
 # Copyright (c) 2022 Omada Community Developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-domain=YOURDOMAIN && \  # Example: yourdomain.com
-host=YOURSUBDOMAIN && \    # this is your subdomain, Example: omada
-password=YOURPASSWORD && \ # this is namecheap ddns password, Example: 7f33f5ad070f257e52d7bcdab12effe4 (check https://ap.www.namecheap.com/Domains/DomainControlPanel/yourdomain.com/advancedns)
-ip=$(dig +short myip.opendns.com @resolver1.opendns.com) && \
-logfile="/var/run/ddns_S{host}.${domain}_namecheap_ipv4.err" && \
+domain=YOURDOMAIN       # Example: yourdomain.com
+host=YOURSUBDOMAIN      # this is your subdomain, Example: omada
+password=YOURPASSWORD   # this is namecheap ddns password, Example: 7f33f5ad070f257e52d7bcdab12effe4 (check https://ap.www.namecheap.com/Domains/DomainControlPanel/yourdomain.com/advancedns)
+ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+logfile="/var/run/ddns_${host}.${domain}_namecheap_ipv4.err"
 /usr/bin/curl -RsS --stderr ${logfile} --noproxy '*' "http://dynamicdns.park-your-domain.com/update?host=${host}&domain=${domain}&password=${password}&ip=${ip}"
